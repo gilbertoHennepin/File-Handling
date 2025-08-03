c2d4b6a99cf5c16035abe4f5c95a2b61667a63aa
gilbertoHennepin <155484528+gilbertoHennepin@users.noreply.github.com>
2025-08-02 19:41:39 -0500

    Update Main.java

--- Implementing file handling/Main.java
+++ Implementing file handling/Main.java
@@ -1,65 +1,43 @@
 import java.io.File;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        // creating book files in myBooks directory
-
-        /*
+        // First create the directory and sample files (run this once)
+        createSampleFiles();
+        
+        // Now uncomment ONE test case at a time:
+        
+        // TEST CASE 1: List files (currently active)
         String directoryPath = "./myBooks";
-        String[] titles = {"The Great Gatsby", "1984", "To Kill a Mockingbird"};
-        String[] authors = {"F. Scott Fitzgerald", "George Orwell", "Harper Lee"};
-
-        for (int i = 0; i < titles.length; i++) {
-            String fileName =  (titles[i]) + ".txt";
-            String content = "Title: " + titles[i] + "\nAuthor: " + authors[i] + "\nSample Content...";
-            FileCreator.createSampleFile(directoryPath, fileName, content);
-        }
+        DirectoryManager.listFilesAndDirectories(directoryPath);
+        
+        /*
+        // TEST CASE 2: Create directory
+        String backupPath = "./myBooksBackup";
+        DirectoryManager.createDirectory(backupPath);
         */
-
-//      Task 1 testing
-
-
-//        String directoryPath = "./myBooks";
-//        DirectoryManager.listFilesAndDirectories(directoryPath);
-
-
-//       Task 2 testing
-
-//        String directoryPath = "./myBooksBackup";
-//        DirectoryManager.createDirectory(directoryPath);
-
-
-//     Task 3 testing
-
-//        String directoryPath = "./myBooksBackup";
-//        String newDirectoryPath = "./BooksBackupDirectory";
-//        DirectoryManager.renameDirectory(directoryPath, newDirectoryPath);
-
-
-//      Task 4 testing
-
-//        String srcPath = "./myBooks";
-//        String destPath = "./BooksBackupDirectory";
-//        DirectoryManager.copyFiles(srcPath, destPath);
-
-
-//     Task 5 testing
-
-//        String filePath = "./myBooks/1984.txt";
-//        DirectoryManager.deleteFile(filePath);
-
-
-//     Task 6 testing
-
-//        Book book = new Book("1984", "George Orwell", "978-0451524935");
-//        String filePath = "./myBooks/book.ser";
-//        Book.serializeBook(book, filePath);
-//        Book deserializedBook = Book.deserializeBook(filePath);
-//        System.out.println("Deserialized Book: " + deserializedBook);
-
-
+        
+        // ... other test cases remain commented ...
     }
 
+    private static void createSampleFiles() {
+        // Create directory if it doesn't exist
+        new File("./myBooks").mkdirs();
+        
+        // Create sample book files
+        String[] titles = {"The Great Gatsby", "1984", "To Kill a Mockingbird"};
+        String[] authors = {"F. Scott Fitzgerald", "George Orwell", "Harper Lee"};
+        
+        for (int i = 0; i < titles.length; i++) {
+            String fileName = "./myBooks/" + titles[i].replace(" ", "_") + ".txt";
+            String content = "Title: " + titles[i] + "\nAuthor: " + authors[i];
+            try {
+                FileWriter writer = new FileWriter(fileName);
+                writer.write(content);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
